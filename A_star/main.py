fb427cbbb4468fad1f184d2dfd5784e567102344
Erlend S <erl1sharpy@gmail.com>
2017-10-10 23:26:05 +0200

    Added basic A_star algorithm

--- A_star/main.py
+++ A_star/main.py
@@ -1,9 +1,9 @@
 from PIL import Image, ImageDraw
-from tk import BoardRender
+from renderboard import BoardRender
+from heapq import heappush, heappop
 
 
 class Board:
-
     def __init__(self, board_name):
         self.board = "boards/board-" + board_name + ".txt"
         self.board_array = []
@@ -29,8 +29,21 @@ class Board:
         img.show()
 
 
-class Astar:
-    pass
+inf = float("inf")
+
+
+# G - the graph, s - the start node, t - the goal, h - the heuristic formula
+def a_star(G, s, t, h):
+    P, Q = {}, [(h(s), None, s)]  # Preds and queue w/heuristic
+    while Q:  # Still unprocessed nodes?
+        d, p, u = heappop(Q)  # Node with lowest heuristic
+        if u in P: continue  # Already visited? Skip it
+        P[u] = p  # Set path predecessor
+        if u == t: return d - h(t), P  # Arrived! Ret. dist and preds
+        for v in G[u]:  # Go through all neighbours
+            w = G[u][v] - h(u) + h(v)  # Modify weight wrt heuristic
+            heappush(Q, (d + w, u, v))  # Add to queue, w/heur as pri
+    return inf, None  # Didn't get to t
 
 
 if __name__ == "__main__":
