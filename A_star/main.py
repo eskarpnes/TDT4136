6f9f260f57849d8f79c6551a8c2edea10b231166
Erlend S <erl1sharpy@gmail.com>
2017-10-13 11:17:20 +0200

    Merged with master

--- A_star/main.py
+++ A_star/main.py
@@ -1,3 +1,4 @@
+from queue import PriorityQueue
 from PIL import Image, ImageDraw
 from renderboard import BoardRender
 
@@ -49,9 +50,32 @@ class Board:
 inf = float("inf")
 
 
-def heuristic(a, b):
-    # Manhattan distance to goal
-    return abs(a.x - b.x) + abs(a.y - b.y)
+def heuristic(goal, curr):
+    #Manhattan distance to goal
+    return abs(curr[0] - goal[0]) + abs(curr[1] - goal[1])
+
+
+def a_star_red_blob(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
 
 
 if __name__ == "__main__":
